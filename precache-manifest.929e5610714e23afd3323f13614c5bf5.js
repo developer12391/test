24db34d83a02c038d66a246fef94cc42bbeaadb8
developer12391 <a.bogelyk1997@gmail.com>
2020-03-05 11:08:40 +0200

    Updates

---
+++ precache-manifest.929e5610714e23afd3323f13614c5bf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b8bf8047eb02c62bb4cf212adbf5e54",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "ea385a7b316c35ed66ef",
+    "url": "/test/static/css/main.e5bd6023.chunk.css"
+  },
+  {
+    "revision": "6e5e64114065e0761b60",
+    "url": "/test/static/js/2.7af209ed.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/test/static/js/2.7af209ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea385a7b316c35ed66ef",
+    "url": "/test/static/js/main.5d07af71.chunk.js"
+  },
+  {
+    "revision": "6bbc3f51202c28fa0a7b",
+    "url": "/test/static/js/runtime-main.5b4329d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
